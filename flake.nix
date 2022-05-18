bdf1b2742e49206ceeeb4252362bfe7b14373760
Mika Naylor <mail@autophagy.io>
2022-05-18 11:33:56 +0200

    Add Flake.nix and Cargo.nix (cargo2nix)

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    cargo2nix.url = "github:cargo2nix/cargo2nix/master";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=release-21.11";
+  };
+
+  outputs = { self, nixpkgs, cargo2nix, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [cargo2nix.overlay];
+        };
+
+        rustPkgs = pkgs.rustBuilder.makePackageSet {
+          rustVersion = "1.60.0";
+          packageFun = import ./Cargo.nix;
+        };
+
+      in rec {
+        packages = {
+          scieldas = (rustPkgs.workspace.scieldas {}).bin;
+        };
+        defaultPackage = packages.scieldas;
+      }
+    );
+}
