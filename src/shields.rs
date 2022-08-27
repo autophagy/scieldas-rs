080b8cd8463405b82f38fcb6fb98b5f2c5c2f263
Mika Naylor <mail@autophagy.io>
2022-08-27 21:34:17 +0200

    More refactoring, move svg/png methods with shield struct

--- src/shields.rs
+++ src/shields.rs
@@ -5,9 +5,6 @@ use rocket::http::ContentType;
 use rocket::request::{FromParam, Request};
 use rocket::response::{self, Responder, Response};
 
-use cached::proc_macro::cached;
-use cached::SizedCache;
-
 use std::path::PathBuf;
 
 pub enum SupportedFiletype {
@@ -21,11 +18,76 @@ pub struct Shield<T: RenderableShield> {
     pub filetype: SupportedFiletype,
 }
 
+impl<T: RenderableShield> Shield<T> {
+    fn to_svg(self) -> String {
+        let value = self.shield.render();
+        let mut svg: String = "".to_string();
+        let width = (&value.len() * 7) + 32;
+
+        let head = format!(
+            r#"<svg baseProfile="full" height="41px" version="1.1" width="{}px" xmlns="http://www.w3.org/2000/svg" xmlns:ev="http://www.w3.org/2001/xml-events" xmlns:xlink="http://www.w3.org/1999/xlink">"#,
+            width
+        );
+        svg.push_str(&head);
+
+        svg.push_str(r##"<rect fill="#2D2D2D" height="100%" width="100%" x="0" y="0" />"##);
+        let b = format!(
+            r##"<text fill="#F2F2F2" font-family="Inconsolata Nerd Font, Inconsolata, monospace" font-size="140" textLength="{}" transform="scale(.1)" x="160" y="240">{}</text>"##,
+            (width * 10) - 320,
+            &value
+        );
+        svg.push_str(&b);
+        svg.push_str("</svg>");
+        svg
+    }
+
+    fn to_png(self) -> Vec<u8> {
+        let svg = self.to_svg();
+        let mut opt = usvg::Options::default();
+        opt.fontdb.load_system_fonts();
+        opt.fontdb.set_monospace_family("Inconsolata");
+
+        let rtree = usvg::Tree::from_str(&svg, &opt.to_ref()).unwrap();
+
+        let pixmap_size = rtree.svg_node().size.to_screen_size();
+        let mut pixmap = tiny_skia::Pixmap::new(pixmap_size.width(), pixmap_size.height()).unwrap();
+        resvg::render(
+            &rtree,
+            usvg::FitTo::Original,
+            tiny_skia::Transform::default(),
+            pixmap.as_mut(),
+        )
+        .unwrap();
+        pixmap.encode_png().unwrap()
+    }
+}
+
 #[rocket::async_trait]
 impl<'r, T: RenderableShield> Responder<'r, 'static> for Shield<T> {
     fn respond_to(self, _: &'r Request<'_>) -> response::Result<'static> {
-        let value = self.shield.render();
-        render_for_filetype(value, self.filetype)
+        match self.filetype {
+            SupportedFiletype::Png => {
+                let png = self.to_png();
+                Response::build()
+                    .header(ContentType::PNG)
+                    .sized_body(png.len(), Cursor::new(png))
+                    .ok()
+            }
+            SupportedFiletype::Svg => {
+                let svg = self.to_svg();
+                Response::build()
+                    .header(ContentType::SVG)
+                    .sized_body(svg.len(), Cursor::new(svg))
+                    .ok()
+            }
+            SupportedFiletype::Txt => {
+                let value = self.shield.render();
+                Response::build()
+                    .header(ContentType::Plain)
+                    .sized_body(value.len(), Cursor::new(value))
+                    .ok()
+            }
+        }
     }
 }
 
@@ -138,72 +200,4 @@ impl RenderableShield for StateShield {
         };
         format!("{}{}{}", prefix, value, suffix)
     }
-}
-
-fn render_for_filetype(value: String, filetype: SupportedFiletype) -> response::Result<'static> {
-    match filetype {
-        SupportedFiletype::Png => {
-            let png = pngify(&svgify(value));
-            Response::build()
-                .header(ContentType::PNG)
-                .sized_body(png.len(), Cursor::new(png))
-                .ok()
-        }
-        SupportedFiletype::Svg => {
-            let svg = svgify(value);
-            Response::build()
-                .header(ContentType::SVG)
-                .sized_body(svg.len(), Cursor::new(svg))
-                .ok()
-        }
-        SupportedFiletype::Txt => Response::build()
-            .header(ContentType::Plain)
-            .sized_body(value.len(), Cursor::new(value))
-            .ok(),
-    }
-}
-
-#[cached(
-    type = "SizedCache<String, Vec<u8>>",
-    create = "{ SizedCache::with_size(1000) }",
-    convert = "{ svg.to_string() }"
-)]
-fn pngify(svg: &str) -> Vec<u8> {
-    let mut opt = usvg::Options::default();
-    opt.fontdb.load_system_fonts();
-    opt.fontdb.set_monospace_family("Inconsolata");
-
-    let rtree = usvg::Tree::from_str(svg, &opt.to_ref()).unwrap();
-
-    let pixmap_size = rtree.svg_node().size.to_screen_size();
-    let mut pixmap = tiny_skia::Pixmap::new(pixmap_size.width(), pixmap_size.height()).unwrap();
-    resvg::render(
-        &rtree,
-        usvg::FitTo::Original,
-        tiny_skia::Transform::default(),
-        pixmap.as_mut(),
-    )
-    .unwrap();
-    pixmap.encode_png().unwrap()
-}
-
-fn svgify(s: String) -> String {
-    let mut svg: String = "".to_string();
-    let width = (s.len() * 7) + 32;
-
-    let head = format!(
-        r#"<svg baseProfile="full" height="41px" version="1.1" width="{}px" xmlns="http://www.w3.org/2000/svg" xmlns:ev="http://www.w3.org/2001/xml-events" xmlns:xlink="http://www.w3.org/1999/xlink">"#,
-        width
-    );
-    svg.push_str(&head);
-
-    svg.push_str(r##"<rect fill="#2D2D2D" height="100%" width="100%" x="0" y="0" />"##);
-    let b = format!(
-        r##"<text fill="#F2F2F2" font-family="Inconsolata Nerd Font, Inconsolata, monospace" font-size="140" textLength="{}" transform="scale(.1)" x="160" y="240">{}</text>"##,
-        (width * 10) - 320,
-        &s
-    );
-    svg.push_str(&b);
-    svg.push_str("</svg>");
-    svg
 }
